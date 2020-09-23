490686ec26de726961a447cf23c02e885891c819
Lasisi <56618398+kenny15335@users.noreply.github.com>
2020-09-23 07:33:06 +0100

    Updates

---
+++ precache-manifest.0b83d8558bbda8df2ddfafff5231ad0a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b6d1c4441a924f854e7801434fc63a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60c991b0bf4c47e4e0e0",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "be95d98f717f977e4348",
+    "url": "/static/css/main.f58b2222.chunk.css"
+  },
+  {
+    "revision": "60c991b0bf4c47e4e0e0",
+    "url": "/static/js/2.62fcbe5c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.62fcbe5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be95d98f717f977e4348",
+    "url": "/static/js/main.47deae68.chunk.js"
+  },
+  {
+    "revision": "9b56b75bec9ec64ab7a7",
+    "url": "/static/js/runtime-main.4feaea7c.js"
+  },
+  {
+    "revision": "6d2f0b6c21c73788638a3c143dae1c1f",
+    "url": "/static/media/Aquaman.6d2f0b6c.jpg"
+  },
+  {
+    "revision": "88e9789f79b6667e69d63fa77821e28f",
+    "url": "/static/media/BlackPanther.88e9789f.jpg"
+  },
+  {
+    "revision": "dec555fda00b2e7a05ae1c764c73b51b",
+    "url": "/static/media/CaptainAmerica.dec555fd.jpg"
+  },
+  {
+    "revision": "73962fc128dceea0ee4a048ae4362ad0",
+    "url": "/static/media/Godzilla.73962fc1.jpg"
+  },
+  {
+    "revision": "f3cfbfafb2979a2fd19aba6850db95d4",
+    "url": "/static/media/Hellboy.f3cfbfaf.jpg"
+  },
+  {
+    "revision": "dfef5800edd3d38b198d8fa6996c2129",
+    "url": "/static/media/Logo.dfef5800.png"
+  },
+  {
+    "revision": "97d5b47be513ac41f601fc61fb1618e3",
+    "url": "/static/media/Spiderman.97d5b47b.jpg"
+  },
+  {
+    "revision": "b40e4b73e7f23d58b1ddb9b39e54a767",
+    "url": "/static/media/Spiderman2.b40e4b73.jpg"
+  },
+  {
+    "revision": "faeab3381b0c0dac4936e3f0243e71d6",
+    "url": "/static/media/TombRaider.faeab338.jpg"
+  },
+  {
+    "revision": "05919d719496f190471e3309d402f8d4",
+    "url": "/static/media/appstore.05919d71.svg"
+  },
+  {
+    "revision": "67963de6e4a3cde046df92360922a859",
+    "url": "/static/media/playstore.67963de6.svg"
+  }
+]);
